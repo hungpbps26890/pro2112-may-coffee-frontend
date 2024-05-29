be62b0b13a9cb1bea1145fe47ee86c5322809388
hungpbps26890 <hungpbps26890@fpt.edu.vn>
2024-05-29 22:49:12 +0700

    Add NavBar and Footer Component

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-white border-bottom sticky-top">
+      <div className="container">
+        <NavLink className="navbar-brand" to={"/home"}>
+          May Coffee
+        </NavLink>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <NavLink className="nav-link" to={"/home"}>
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to={"/menu"}>
+                Menu
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to={"/cart"}>
+                Cart
+              </Link>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Order
+              </a>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Welcome, Username
+              </a>
+              <ul className="dropdown-menu">
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Log in
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Register
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Profile
+                  </a>
+                </li>
+                <li>
+                  <hr className="dropdown-divider" />
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Log out
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
