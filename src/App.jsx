24b8f3d15e00924ea28d2ca1604e877905d4619f
hungpbps26890 <hungpbps26890@fpt.edu.vn>
2024-05-29 23:07:50 +0700

    Add Page Menu Component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home/Home";
+import Menu from "./pages/Menu/Menu";
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/home" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/menu/category/:id" element={<Menu />} />
       </Routes>
       <Footer />
     </>
